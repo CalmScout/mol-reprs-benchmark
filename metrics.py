99e545ea5a83e89f4eb4dedb6b92b71cdb9b988d
Anton <popovanton567@gmail.com>
2024-06-12 19:13:55 +0200

    refactor: not save X and model in ClassificationMetrics

--- metrics.py
+++ metrics.py
@@ -23,7 +23,6 @@ class ClassificationMetrics:
         """
         assert "class_label" in df.columns and "split" in df.columns
         self.target_id = target_id
-        self.model = model
 
         df_train = df.filter(pl.col("split") == "train")
         df_val = df.filter(pl.col("split") == "validation")
@@ -31,13 +30,13 @@ class ClassificationMetrics:
 
         feature_cols = [f for f in df.columns if f.startswith("f_")]
 
-        self.X_train, self.y_train = df_train[feature_cols], df_train["class_label"]
-        self.X_val, self.y_val = df_val[feature_cols], df_val["class_label"]
-        self.X_test, self.y_test = df_test[feature_cols], df_test["class_label"]
+        X_train, self.y_train = df_train[feature_cols], df_train["class_label"]
+        X_val, self.y_val = df_val[feature_cols], df_val["class_label"]
+        X_test, self.y_test = df_test[feature_cols], df_test["class_label"]
 
-        self.y_train_pred = self.model.predict(self.X_train)
-        self.y_val_pred = self.model.predict(self.X_val)
-        self.y_test_pred = self.model.predict(self.X_test)
+        self.y_train_pred = model.predict(X_train)
+        self.y_val_pred = model.predict(X_val)
+        self.y_test_pred = model.predict(X_test)
 
         # calculate matthews correlation coefficient
         self.mcc_train = self.mcc(self.y_train, self.y_train_pred)
